528bb246ebc9f905482c444808632657d0f0068f
shuntashinkura <sccftddn@gmail.com>
2025-12-05 19:10:22 +0900

    [在庫管理]基礎

---
+++ zaikokanri/src/main/java/com/example/demo/ZaikokanriApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaikokanriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaikokanriApplication.class, args);
+	}
+
+}
